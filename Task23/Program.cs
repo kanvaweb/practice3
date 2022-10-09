4914c09f3341574c4ac54a37047e5461710534b5
Andrey Kislitsyn <kanvaweb@gmail.com>
2022-10-09 16:22:51 +0700

    Task 23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Задача 23. Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write($"{N} -> ");
+
+for (int i=1; i<=N; i++) 
+{
+    if (i < N)
+        Console.Write(Math.Pow(i, 3) + ", ");
+    else
+        Console.Write(Math.Pow(i, 3));
+}
